4e404af6cd95f77ff942800eafe6d9a39151c36f
daajdk <mccarthyk1@students.rowan.edu>
2013-11-06 22:07:59 -0500

    testing

--- test.java
+++ test.java
@@ -3,22 +3,10 @@ public class test {
 
 	public static void main(String[] args) {
 		SATSudoku sat = new SATSudoku();
-
-		int [] [] sudoku = new int[] [] {
-                    {0,0,0,0,0,0,0,0,0,0},
-				  { 0,0,0,7,0,0,0,1,0,0},
-				  { 0,0,0,0,5,8,0,9,4,0},
-			      { 0,9,0,8,0,0,0,0,3,0},
-			      { 0,0,0,4,0,5,6,0,0,1},
-			      { 0,0,0,0,0,0,0,0,0,0},
-                              {0,6,0,0,7,1,0,4,0,0},
-                              {0,0,3,0,0,0,0,7,0,6},
-                              {0,0,4,6,0,2,1,0,0,0},
-                              {0,0,0,5,0,0,0,8,0,0}
-			    } ;
+        ScanBoard board = new ScanBoard(10);
+        int[][] sudoku = board.getBoard(args[0]);
 		String str = sat.buildClauses(sudoku);
-                //System.out.println("TEST");
-		System.out.println("p cnf "+sat.varNum+" "+sat.clauseNum);
+        System.out.println("p cnf "+sat.varNum+" "+sat.clauseNum);
 		System.out.println(str);
 
 	}
